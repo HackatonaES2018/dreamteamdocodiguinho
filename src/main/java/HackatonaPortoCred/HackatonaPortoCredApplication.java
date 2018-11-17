8322ee82ba398415822f0274fb0937ab78024527
gutos <augusto.klaic@acad.pucrs.br>
2018-11-17 14:16:01 -0200

    Initial Commit

---
+++ src/main/java/HackatonaPortoCred/HackatonaPortoCredApplication.java
@@ -0,0 +1,12 @@
+package HackatonaPortoCred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonaPortoCredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonaPortoCredApplication.class, args);
+	}
+}
